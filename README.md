2cc3dd918f7cbddd423c26cebdeff6eb4157bfab
Matthew Davis <matthew@matthewdavis.io>
2025-07-01 20:18:12 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# @mateothegreat/svelte5-router example
+
+This is a simple example of how to use the @mateothegreat/svelte5-router package.
+
+<div align="center">
+  <a href="https://stackblitz.com/github/mateothegreat/svelte5-router-example">
+    <img src="https://developer.stackblitz.com/img/open_in_stackblitz.svg" alt="Open in StackBlitz" />
+  </a>
+</div>
+
+## Running the example
+
+```bash
+npm install
+npm run dev
+```
---
+++ src/app.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import {
+    route,
+    Router,
+    type RouteConfig,
+  } from "@mateothegreat/svelte5-router";
+  import Home from "./routes/home.svelte";
+  import Kitchen from "./routes/kitchen.svelte";
+  const routes: RouteConfig[] = [
+    {
+      path: "/",
+      component: Home,
+    },
+    {
+      path: "/kitchen",
+      component: Kitchen,
+    },
+  ];
+</script>
+
+<a use:route href="/">Home</a> |
+<a use:route href="/kitchen">Kitchen</a>
+
+<Router {routes} />
